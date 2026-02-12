f5f7cf512ad72602f6d5d90cb9a64c193683acd8
Alfredo Mujica <126611336+jamrpro249@users.noreply.github.com>
2026-02-12 01:57:44 +0000

    Contador simple completado

--- src/js/components/number.jsx
+++ src/js/components/number.jsx
@@ -1,4 +1,6 @@
 import '../components/css/number.css';
+
+
 const Numero = (props) => {
     return (
         <div className="numberPrincipal">
@@ -6,8 +8,12 @@ const Numero = (props) => {
                 <div className='fontDigital'><i className="bi bi-alarm"></i></div>
             <div className='fontDigital'><h2>{props.Numeros[0]}</h2></div>
             <div className='fontDigital'><h2>{props.Numeros[1]}</h2></div>
+            <h2 className="mx-1">:</h2>
             <div className='fontDigital'><h2>{props.Numeros[2]}</h2></div>
             <div className='fontDigital'><h2>{props.Numeros[3]}</h2></div>
+            <h2 className="mx-1">:</h2>
+            <div className='fontDigital'><h2>{props.Numeros[4]}</h2></div>
+            <div className='fontDigital'><h2>{props.Numeros[5]}</h2></div>
             </div>
         </div>
     );
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -16,17 +16,27 @@ let counter = 0;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 setInterval(() => {
-  const digitos = counter.toString().padStart(4, '0').split("");
+  const totalSeconds = counter;
+
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+  const seconds = totalSeconds % 60;
+
+  const timeString =
+    hours.toString().padStart(2, '0') +
+    minutes.toString().padStart(2, '0') +
+    seconds.toString().padStart(2, '0');
+
+  const digitos = timeString.split("");
 
   root.render(
     <React.StrictMode>
       <div className='contenedorPrincipal'>
         <Numero Numeros={digitos} />
       </div>
+
     </React.StrictMode>
   );
 
   counter++;
 }, 1000);
-
-
